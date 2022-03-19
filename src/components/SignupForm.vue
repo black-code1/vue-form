56f1458c9ccd8c2d25451818affc17b397e317ef
black-code1 <legrandmahamat1@gmail.com>
2022-03-19 13:12:30 +0100

    Deleting Skills

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -15,7 +15,9 @@
     <label>Skills</label>
     <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
 
-    <div v-for="skill in skills" :key="skill" class="pill">{{ skill }}</div>
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
     <div class="terms">
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
@@ -47,6 +49,11 @@ export default {
         }
         this.tempSkill = "";
       }
+    },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return skill !== item;
+      });
     },
   },
 };
@@ -86,5 +93,17 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
