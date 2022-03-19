aa46083f3bb7c70a7ec2f873f58e62f13ca51493
black-code1 <legrandmahamat1@gmail.com>
2022-03-19 12:48:14 +0100

    Checkboxes

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -11,10 +11,16 @@
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>Accept terms and conditions</label>
+    </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
+  <p>Terms: {{ terms }}</p>
 </template>
 
 <script>
@@ -24,6 +30,7 @@ export default {
       email: "",
       password: "",
       role: "designer",
+      terms: false,
     };
   },
 };
@@ -56,5 +63,12 @@ select {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
